c00e66b2d6c89b9beaf58c39e9dc115ffa48af58
MuhammadAlvitoAditya <itoa47@gmail.com>
2022-07-29 13:09:24 +0700

    port

--- src/index.js
+++ src/index.js
@@ -83,8 +83,8 @@ app.use(function (err, req, res) {
   res.render("error");
 });
 
-// app.listen(port, () => {
-// 	console.log(`App run on :${port}`)
-// })
+app.listen(process.env.PORT || '3000', () => {
+	console.log(`App run on :`)
+})
 
 module.exports = app;
